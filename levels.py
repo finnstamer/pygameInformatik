757b337256a6c33efc77d345ca054c8d2e69fcba
Leonhkm <Leonkrumke@gmail.com>
2021-11-09 17:18:45 +0100

    Background2

--- levels.py
+++ levels.py
@@ -17,15 +17,18 @@ playerGroup = Group[Player]("player", Player).add(player)
 wall11 = Wall()
 wall12= Wall()
 
+bg = Backround("images/Dschungel.png")
+backgroundGroup = Group("bg", Backround).add(bg)
+
 wall11.updatePos(pygame.Vector2(200, 100))
 wall12.updatePos(pygame.Vector2(275, 150))
 wallGroup1 = Group[Wall]('walls', Wall).add(wall11, wall12)
-level1 = Level(1, [playerGroup, wallGroup1])
+level1 = Level(1, [backgroundGroup, playerGroup, wallGroup1])
 # Level2
 #-------------------
 wall21 = Wall()
 wall22 = Wall()
-bg = Backround("images/pacman.png")
+bg = Backround("images/Wasser.png")
 backgroundGroup = Group("bg", Backround).add(bg)
 
 wall21.updatePos(pygame.Vector2(150, 100))
