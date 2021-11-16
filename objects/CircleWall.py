eb8dc0cf6cfc312108eca2d4e05c8beeb257d6d7
Leonhkm <Leonkrumke@gmail.com>
2021-11-16 16:57:22 +0100

    CircleWall

---
+++ objects/CircleWall.py
@@ -0,0 +1,12 @@
+import pygame
+from base.object.GameObject import GameObject
+
+
+class CircleWall (GameObject):
+      def __init__(self) -> None:
+        super().__init__()
+        self.solid = True
+        self.color = (100, 100, 100)
+        self.pos = pygame.math.Vector2(2,8)
+        self.altRect = pygame.draw.circle(100,100,255,15)
+
