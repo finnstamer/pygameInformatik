57cc5bcb74ad3c7f4f469895c8b08f273f04533b
code-smpl <smplcode.instagram@gmail.com>
2022-01-10 15:47:01 +0100

    Level

--- levels.py
+++ levels.py
@@ -26,26 +26,31 @@ class Level1(AbstractLevel):
         super().__init__(1)
     def make(self):
         mB = MapBuilder()
-        for i in range(10):
-            mB.addObject(Wall(Vector2(randrange(0, 500), randrange(0, 500)), 25, 25))
-
-        player = Player()
-        mB.placeInCenter(player)
-
-        projectile = Projectile(range=500, damage=10, speed=20, width=5, height=5, relativePosition=Vector2(75, 3))
-        projectile.color = (114, 114, 114)
-        weapon = Weapon(player, Vector2(-10, 10), projectile, cooldown=100, munition=50000)
-        weapon.color = (29, 191, 172)
-        weapon.height = 20
-        weapon.width = 75
-        weapon.setImage("images/pump.png")
-        weapon.setAlias("weapon")
-
-        mB.addObject(weapon, projectile)
-
-        ektoplasma = Ektoplasma().setAlias("ekto1")
-        mB.nextTo(player, ektoplasma, 1, 0, marginX=0)
+        objects = [
+          Wall(Vector2(), width=5, height=5)
+        ]
+        mB.addObject(*objects)
         self.objects = mB.objects
+
+        # for i in range(10):
+        #     mB.addObject(Wall(Vector2(randrange(0, 500), randrange(0, 500)), 25, 25))
+
+        # player = Player()
+        # mB.placeInCenter(player)
+
+        # projectile = Projectile(range=500, damage=10, speed=20, width=5, height=5, relativePosition=Vector2(75, 3))
+        # projectile.color = (114, 114, 114)
+        # weapon = Weapon(player, Vector2(-10, 10), projectile, cooldown=100, munition=50000)
+        # weapon.color = (29, 191, 172)
+        # weapon.height = 20
+        # weapon.width = 75
+        # weapon.setImage("images/pump.png")
+        # weapon.setAlias("weapon")
+
+        # mB.addObject(weapon, projectile)
+
+        # ektoplasma = Ektoplasma().setAlias("ekto1")
+        # mB.nextTo(player, ektoplasma, 1, 0, marginX=0)
 
 # mB.pointMirror(player.cRect.corners[0], player, ektoplasma)
 # mB.axisMirror(-1, player.pos.y, player, ektoplasma)
